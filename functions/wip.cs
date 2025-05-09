bac3fe7481462e41f3359e8749e236a87d18fe61
Thomas Lunnen III <60558126+wLunnen@users.noreply.github.com>
2025-05-09 14:14:15 -0700

    Update and rename index.cs to wip.cs

---
+++ functions/wip.cs
@@ -0,0 +1,13 @@
+//include C# in this to add diversity to sites
+
+//class structures include 
+artists - albums, songs
+albums - songs, covers
+songs - lyrics
+
+//add request feature for sampling/colab
+
+//download (for a fee)
+//advertisements of new artists, artist album/single releases
+
+//add licensing, but include specific verbage for artists to create their own legal guidelines regarding their music (we could determine the legalese/represent artists in the event of theft)
