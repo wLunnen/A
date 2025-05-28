294406fc2a500d5fd7b96ff5c95a4612ae29370c
Thomas Lunnen III <60558126+wLunnen@users.noreply.github.com>
2025-05-28 16:13:17 -0700

    Update funcitons.js

--- functions/funcitons.js
+++ functions/funcitons.js
@@ -214,3 +214,20 @@ function showNextImage() {
 
 setInterval(showNextImage, 3000); // Change image every 3 seconds
 */
+
+//menu
+function openLeftMenu() {
+  document.getElementById("leftMenu").style.display = "block";
+}
+
+function closeLeftMenu() {
+  document.getElementById("leftMenu").style.display = "none";
+}
+
+function openRightMenu() {
+  document.getElementById("rightMenu").style.display = "block";
+}
+
+function closeRightMenu() {
+  document.getElementById("rightMenu").style.display = "none";
+}
